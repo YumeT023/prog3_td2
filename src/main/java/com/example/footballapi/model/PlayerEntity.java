b865081d9c57412a4f90d0521545ce894e57b7b6
Yume <hei.hajatiana@gmail.com>
2022-12-18 12:25:06 +0300

    feat(q2): PlayerEntity

---
+++ src/main/java/com/example/footballapi/model/PlayerEntity.java
@@ -0,0 +1,37 @@
+package com.example.footballapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "player")
+public class PlayerEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private TeamEntity team;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int number;
+}
