bb39e3f9fe608b6e39332c5552cd62306fea9cb6
Yume <hei.hajatiana@gmail.com>
2022-12-18 11:16:32 +0300

    chore(q1): team migration

---
+++ src/main/resources/db/migration/V0_4__Insert_default_play_against_team.sql
@@ -0,0 +1,5 @@
+INSERT INTO team_play_against (datetime, stadium, team1_id, team2_id)
+VALUES ('2022-12-19', 'stadium-1', 1, 2),
+       ('2022-12-20', 'stadium-2', 2, 3),
+       ('2022-12-20', 'stadium-2', 3, 1);
+
