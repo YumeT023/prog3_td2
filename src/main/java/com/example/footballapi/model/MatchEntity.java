6e6da47eb35d4e7441f0475da1a3b5a0f85c1491
Yume <hei.hajatiana@gmail.com>
2022-12-18 15:00:29 +0300

    feat(q2): MatchEntity

---
+++ src/main/java/com/example/footballapi/model/MatchEntity.java
@@ -0,0 +1,44 @@
+package com.example.footballapi.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "team_play_against")
+public class MatchEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private LocalDateTime datetime;
+
+    @Column(nullable = false)
+    private String stadium;
+
+    @OneToOne
+    @JoinColumn(name = "team1_id")
+    private TeamEntity firstTeam;
+
+    @OneToOne
+    @JoinColumn(name = "team2_id")
+    private TeamEntity secondTeam;
+}
