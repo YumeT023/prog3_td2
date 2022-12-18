a1bfff35d239b61d4311cfff061c9ac2e1fd399c
Yume <hei.hajatiana@gmail.com>
2022-12-18 23:48:50 +0300

    fix(q2): team have sponsor

--- src/main/java/com/example/footballapi/model/TeamEntity.java
+++ src/main/java/com/example/footballapi/model/TeamEntity.java
@@ -4,11 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -25,4 +30,12 @@ public class TeamEntity {
 
     @Column(nullable = false)
     private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "team_have_sponsor",
+            joinColumns = @JoinColumn(name = "team_id"),
+            inverseJoinColumns = @JoinColumn(name = "sponsor_id")
+    )
+    private List<SponsorEntity> sponsors;
 }
